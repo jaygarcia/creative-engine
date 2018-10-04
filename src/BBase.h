7945ae56294e867a1e7ec02a401d6d3e2014b2e9
Jay Garcia <jesusdgarcia@gmail.com>
2018-10-04 16:20:00 -0400

    Making sure BBase.h is in alignment with master

--- src/BBase.h
+++ src/BBase.h
@@ -1,109 +1,112 @@
-#include "BBase.h"
+#ifndef BBASE_H
+#define BBASE_H
 
-#ifndef __XTENSA__
+#include "BTypes.h"
 
-#include <stdlib.h>
-#include <inttypes.h>
-#include <time.h>
-#include <math.h>
-
-#else
-
-#include "esp_system.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
-#endif
-
-BBase::BBase() {
-  //
-}
-
-BBase::~BBase() {
-  //
-}
-
-/*
- * Random number generator
- * http://www.firstpr.com.au/dsp/rand31/p1192-park.pdf
- *
- * See also: random-number-generator.pdf in references/
+/**
+ * Base class for EVERYTHING.
  */
-static TUint32 sRandomSeed;
 
-void SeedRandom(TUint32 aSeed) {
-  sRandomSeed = aSeed;
-}
-
-TUint32 Random() {
-  static const TUint32 a = 16807,
-          m = 2147483647;
-  sRandomSeed = (a * sRandomSeed) % m;
-  return sRandomSeed % m;
-}
-
-TInt32 Random(TInt32 aMin, TInt32 aMax) {
-  return TInt32(Random()) % (aMax - aMin) + aMin;
-}
-
-TFloat RandomFloat() {
-  TInt32 r = TInt32(Random());
-  TFloat ret = TFloat(r) / TFloat(UINT32_MAX);
-  return ret;
-}
-// Global Versions
-TAny *AllocMem(size_t size, TUint16 aType) {
 #ifdef __XTENSA__
-  return heap_caps_malloc(size, aType);
+#include "freertos/FreeRTOS.h"
+#define MEMF_SLOW (MALLOC_CAP_8BIT | MALLOC_CAP_SPIRAM)
+#define MEMF_FAST (MALLOC_CAP_8BIT | MALLOC_CAP_DMA)
+
 #else
-  return malloc(size);
+
+#define MEMF_SLOW 0
+#define MEMF_FAST (1 << 0)
+
 #endif
-}
 
-void FreeMem(TAny *ptr) { free(ptr); }
+class BBase {
+public:
+    BBase();
 
-TAny *ReallocMem(TAny *aPtr, size_t aSize, TInt16 aType) {
+    ~BBase();
+};
+
+//////// RANDOM NUMBER GENERATOR
+
+/**
+ * SeedRandom
+ *
+ * Seed random number generator.
+ *
+ * @param aSeed
+ */
+void SeedRandom(TUint32 aSeed);
+
+/**
+ * Generate a random number
+ * @return
+ */
+TUint32 Random();
+
+/**
+ * Generate a random number in given range.
+ *
+ * Note: this routine produces signed result and takes signed arguments.  This allows a range of something like -10 to 100.
+ *
+ * @param aMin
+ * @param aMax
+ * @return
+ */
+TInt32 Random(TInt32 aMin, TInt32 aMax);
+
+/**
+ * Return random number between 0 and 1 (floating point)
+ * @return
+ */
+TFloat RandomFloat();
+
+/**
+ * Allocate memory of specified size and type
+ *
+ * @param aSize
+ * @param aFlags MEMF_FAST or MEMF_SLOW
+ * @return
+ */
+extern TAny *AllocMem(size_t aSize, TUint16 aFlags);
+
+/**
+ * Free memory allocated with AllocMem
+ * @param ptr
+ */
+extern void FreeMem(TAny *ptr);
+
+/**
+ * Resize memory allocated with AllocMem
+ */
+extern TAny *ReallocMem(TAny *ptr, size_t aSize);
+
 #ifdef __XTENSA__
-  return heap_caps_realloc(aPtr, aSize, aType);
-#else
-  return realloc(aPtr, aSize);
-#endif
-}
+inline void *operator new(size_t size) { return AllocMem(size, MEMF_SLOW); }
 
-TUint32 Milliseconds() {
-#ifdef __XTENSA__
-  return (TUint32)(esp_timer_get_time() / 1000);
-#else
-  TUint32         ms;
-  time_t          s;
-  struct timespec spec;
+inline void *operator new[](size_t size) { return AllocMem(size, MEMF_SLOW); }
 
-  clock_gettime(CLOCK_REALTIME, &spec);
-  s  = spec.tv_sec;
-  ms = lround(spec.tv_nsec / 1.0e6);
-  if (ms > 999) {
-    s++;
-    ms = 0;
-  }
-  ms += s * 1000;
-  return ms;
-#endif
-}
-
-#ifndef __XTENSA__
-
-void *operator new(size_t size) { return AllocMem(size, MEMF_SLOW); }
-
-void *operator new[](size_t size) { return AllocMem(size, MEMF_SLOW); }
-
-void operator delete(void *ptr) {
+inline void operator delete(void *ptr) {
   //
   FreeMem(ptr);
 }
 
-void operator delete[](void *ptr) {
+inline void operator delete[](void *ptr) {
   //
   FreeMem(ptr);
 }
+#else
+
+// Cannot inline on the host (defined in BBase.cpp)
+extern void *operator new(size_t size);
+
+extern void *operator new[](size_t size);
+
+extern void operator delete(void *ptr);
+
+extern void operator delete[](void *ptr);
+
+#endif
+
+extern TUint32 Milliseconds();
 
 #endif
